656a7feb45576f761341b3e151aaae550f27857f
Moeka <moeka@MoekanoMacBook-Air.local>
2017-10-29 14:26:05 +0900

    Initial Commit

---
+++ benkyoapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  benkyoapp
+//
+//  Created by Moeka on 2017/10/29.
+//  Copyright © 2017年 Moeka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
